30a840d3bb9231536ae899340747980dc43da9e4
Jose Alejandro Jiménez Vásquez <140639167+ElMasLindoDeEafit@users.noreply.github.com>
2023-09-17 22:51:08 -0500

    Update DjEmocional.java

--- DjEmocional.java
+++ DjEmocional.java
@@ -1,25 +1,33 @@
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Random;
 
 public class DjEmocional extends JFrame implements ActionListener {
-    private JLabel textoInicio, textoObjetivo, textoGenero, textoTiempo;
+    private JLabel textoInicio, textoObjetivo, textoGenero;
     private JComboBox<String> selectorEmocion, selectorObjetivo, selectorGenero;
     private JButton enviarButton, omitirButton;
     private JTextArea recomendacionArea;
-    private JSpinner spinnerTiempo;
     private JSlider calificacionSlider;
     private JLabel calificacionLabel;
+    private HashMap<String, String> playlists; // Almacena playlists por combinaciones de emociones y objetivos.
 
     public DjEmocional() {
         super("Dj Emocional");
 
-        // Preparación de componentes
+        // Datos ficticios de playlists. En una implementación real, este sería un conjunto mucho más grande y se obtendría de una base de datos o API.
+        playlists = new HashMap<>();
+        playlists.put("Felicidad_Intensificar_Rock", "Playlist de Rock Alegre");
+        //... [Agregar más playlists según corresponda]
+
+        
         String[] emociones = {"Felicidad", "Tristeza", "Enojo", "Ansiedad", "Estrés"};
         String[] objetivos = {"Intensificar", "Mantener", "Cambiar"};
         String[] generos = {"Rock", "Jazz", "Pop", "Electrónica", "Clásica"};
 
-        // Creación de componentes
+
         textoInicio = new JLabel("¿Qué emoción sientes?");
         textoObjetivo = new JLabel("¿Qué quieres hacer con tu emoción?");
         textoGenero = new JLabel("Selecciona tu género musical favorito");
@@ -31,21 +39,17 @@ public class DjEmocional extends JFrame implements ActionListener {
         recomendacionArea = new JTextArea(5, 30);
         recomendacionArea.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(recomendacionArea);
-
-        textoTiempo = new JLabel("¿Cuántos minutos quieres escuchar?");
-        spinnerTiempo = new JSpinner(new SpinnerNumberModel(30, 1, 240, 1));
-
         calificacionLabel = new JLabel("Califica la playlist (1-10):");
         calificacionSlider = new JSlider(1, 10, 5);
         calificacionSlider.setMajorTickSpacing(1);
         calificacionSlider.setPaintTicks(true);
         calificacionSlider.setPaintLabels(true);
 
-        // Registro de eventos
+ 
         enviarButton.addActionListener(this);
-        omitirButton.addActionListener(e -> recomendacionArea.setText("¡Okay! Generando otra recomendación...")); // Simplificado
+        omitirButton.addActionListener(e -> generarRecomendacion()); 
 
-        // Configuración del panel y adición de componentes
+
         setLayout(new FlowLayout());
         add(textoInicio);
         add(selectorEmocion);
@@ -53,8 +57,6 @@ public class DjEmocional extends JFrame implements ActionListener {
         add(selectorObjetivo);
         add(textoGenero);
         add(selectorGenero);
-        add(textoTiempo);
-        add(spinnerTiempo);
         add(enviarButton);
         add(omitirButton);
         add(scrollPane);
@@ -68,14 +70,16 @@ public class DjEmocional extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        generarRecomendacion();
+    }
+
+    private void generarRecomendacion() {
         String emocion = (String) selectorEmocion.getSelectedItem();
         String objetivo = (String) selectorObjetivo.getSelectedItem();
         String genero = (String) selectorGenero.getSelectedItem();
-        int minutos = (int) spinnerTiempo.getValue();
+        String key = emocion + "_" + objetivo + "_" + genero;
 
-        String recomendacion = "Basado en tu emoción " + emocion + ", tu deseo de " + objetivo + ", tu preferencia por "
-                + genero + " y por " + minutos + " minutos, te recomendamos la playlist: ...";
-
+        String recomendacion = playlists.getOrDefault(key, "No tenemos una playlist específica para tu selección. Pero aquí hay una sugerencia genérica para ti: ...");
         recomendacionArea.setText(recomendacion);
     }
 
